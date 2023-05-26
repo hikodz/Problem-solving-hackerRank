3716be34632da1941777743d1550f6b72590379a
mohamed rida <119520140+hiko2002l@users.noreply.github.com>
2023-05-26 18:06:02 +0100

    My First Qustion

---
+++ Question- Function.py
@@ -0,0 +1,17 @@
+
+#! Qustion hackerrank :
+v = None
+my_range = list(range(v))
+print(sum(my_range, v) + pow(v, v, v)) #820
+
+#* My result :
+def num_failed(the_number_failed):
+    my_num_result = list(range(1, the_number_failed)) # the number result can't accpect in reange and 0 number
+    for number in my_num_result:
+        my_range = list(range(number))
+        if sum(my_range, number) + pow(number, number, number) == the_number_failed:
+            return number
+
+num_failed = num_failed(820)
+print(num_failed)
+
